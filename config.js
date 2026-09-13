044441f8d0b838dad0c458f18bfbfa708abdde8e
Xarakson <xarakson123@gmail.com>
2026-09-13 17:15:45 +0200

    Wczytaj poprawki rozgrywki i wyglądu talii

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 document.write('<script src="config-base.js"><\/script>');
 document.write('<script src="social-sync.js"><\/script>');
 document.write('<script src="social.js"><\/script>');
+document.write('<script src="enhancements.js"><\/script>');
