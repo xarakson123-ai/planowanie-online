27470892a9e114138c798e4975172aed9e5f29a8
Xarakson <xarakson123@gmail.com>
2026-09-13 17:16:23 +0200

    Wczytaj poprawkę czatu znajomych

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ document.write('<script src="config-base.js"><\/script>');
 document.write('<script src="social-sync.js"><\/script>');
 document.write('<script src="social.js"><\/script>');
 document.write('<script src="enhancements.js"><\/script>');
+(function(){var x=document.createElement('script');x.src='social-fix.js';document.head.appendChild(x)})();
