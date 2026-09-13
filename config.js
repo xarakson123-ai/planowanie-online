c8938b43a787045c6e737ac43ee924bf2af58308
Xarakson <xarakson123@gmail.com>
2026-09-13 22:38:13 +0200

    Load v10 stake UI

--- config.js
+++ config.js
@@ -3,9 +3,5 @@ document.write('<script src="social-sync.js"><\/script>');
 document.write('<script src="social.js"><\/script>');
 document.write('<script src="enhancements.js"><\/script>');
 document.write('<script src="fix-v10.js"><\/script>');
-(function(){
-  window.addEventListener('load',function(){
-    document.body.style.pointerEvents='auto';
-    document.querySelectorAll('.uiModal,.v8Overlay').forEach(function(e){e.remove();});
-  });
-})();
+document.write('<script src="stake-ui.js"><\/script>');
+(function(){window.addEventListener('load',function(){document.body.style.pointerEvents='auto';document.querySelectorAll('.uiModal,.v8Overlay').forEach(function(e){e.remove()});})})();
