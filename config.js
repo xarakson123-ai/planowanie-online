31c12c0cef8c296a28524349776fca9f839c641a
Xarakson <xarakson123@gmail.com>
2026-09-13 17:42:44 +0200

    Dodaj muzyke stawki reklamy profil i ustawienia

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ document.write('<script src="config-base.js"><\/script>');
 document.write('<script src="social-sync.js"><\/script>');
 document.write('<script src="social.js"><\/script>');
 document.write('<script src="enhancements.js"><\/script>');
+document.write('<script src="extras.js"><\/script>');
 (function(){var x=document.createElement('script');x.src='social-fix.js';document.head.appendChild(x)})();
