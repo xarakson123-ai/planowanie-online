f8a0ef1de7f3ade65e1a8c2b30d4354def63dd25
Xarakson <xarakson123@gmail.com>
2026-09-13 17:44:54 +0200

    Dolacz finalne poprawki stawki i profilu

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ document.write('<script src="social.js"><\/script>');
 document.write('<script src="enhancements.js"><\/script>');
 document.write('<script src="extras.js"><\/script>');
 (function(){var x=document.createElement('script');x.src='social-fix.js';document.head.appendChild(x)})();
+(function(){var x=document.createElement('script');x.src='final-fix.js';document.head.appendChild(x)})();
