88475675c97c476400877a03eefc306b92fe236d
Xarakson <xarakson123@gmail.com>
2026-09-13 22:36:06 +0200

    Load stable v10 fixes

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ document.write('<script src="config-base.js"><\/script>');
 document.write('<script src="social-sync.js"><\/script>');
 document.write('<script src="social.js"><\/script>');
 document.write('<script src="enhancements.js"><\/script>');
+document.write('<script src="fix-v10.js"><\/script>');
 (function(){
   window.addEventListener('load',function(){
     document.body.style.pointerEvents='auto';
